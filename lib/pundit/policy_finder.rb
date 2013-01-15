8c599fdd3c03b5eef7da224f9a56e05321a1ed10
Ben Caldwell <bencaldwell@einti.com>
2013-01-15 10:17:28 +0800

    Use #safe_constantize, simplify Scope resolution

--- lib/pundit/policy_finder.rb
+++ lib/pundit/policy_finder.rb
@@ -19,43 +19,23 @@ module Pundit
     end
 
     def scope
-      policy_ancestor_names.reduce(nil) do |memo, pan|
-        begin
-          memo || "#{pan}::Scope".constantize
-        rescue NameError
-          nil
-        end
-      end
+      policy.const_get(:Scope) if policy
     end
 
     def policy
-      policy_name.constantize
-    rescue NameError
-      nil
+      policy_name.safe_constantize
     end
 
     def scope!
-      scope or raise NotDefinedError, "unable to find scope #{scope_name} for #{object}"
+      scope or raise NotDefinedError, "unable to find scope for #{object}"
     end
 
     def policy!
       policy or raise NotDefinedError, "unable to find policy #{policy_name} for #{object}"
     end
 
-    def scope_name
-      "#{name}Policy::Scope"
-    end
-
     def policy_name
       "#{name}Policy"
-    end
-
-    def policy_ancestor_names
-      if policy.present?
-        policy.ancestors.map(&:name).select{ |s| s =~ /Policy$/ }
-      else
-        [policy_name]
-      end
     end
   end
 end
