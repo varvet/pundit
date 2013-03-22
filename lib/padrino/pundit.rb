5b38376c86b25b27a77e4cedd9594d585baa0070
Stefano Verna <stefano.verna@welaika.com>
2013-03-23 00:10:12 +0100

    Padrino 0.11

--- lib/padrino/pundit.rb
+++ lib/padrino/pundit.rb
@@ -9,11 +9,7 @@ module Padrino
 
     module Adapter
       def action_name
-        controller_name = request.route_obj.controller.to_s
-        name = request.route_obj.named.to_s
-        name.gsub!(/^#{controller_name}_?/, '')
-        name = 'index' if name == ''
-        name
+        action
       end
     end
   end
