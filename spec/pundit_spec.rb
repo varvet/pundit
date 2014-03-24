8c2d87af2e2551e86ee683a144ba5b6dc336498a
Roman Heinrich <roman.heinrich@gmail.com>
2014-03-24 12:53:03 +0100

    nest policies in AS (activesupport) module

--- spec/pundit_spec.rb
+++ spec/pundit_spec.rb
@@ -4,56 +4,57 @@ require "active_support/core_ext"
 require "active_model/naming"
 require 'spec_helper'
 
-class PostPolicy < Struct.new(:user, :post)
-  def update?
-    post.user == user
+module AS
+  class PostPolicy < Struct.new(:user, :post)
+    def update?
+      post.user == user
+    end
+    def destroy?
+      false
+    end
+    def show?
+      true
+    end
   end
-  def destroy?
-    false
+  class PostPolicy::Scope < Struct.new(:user, :scope)
+    def resolve
+      scope.published
+    end
   end
-  def show?
-    true
+  class Post < Struct.new(:user)
+    def self.published
+      :published
+    end
   end
-end
-class PostPolicy::Scope < Struct.new(:user, :scope)
-  def resolve
-    scope.published
-  end
-end
-class Post < Struct.new(:user)
-  def self.published
-    :published
-  end
-end
 
-class CommentPolicy < Struct.new(:user, :comment); end
-class CommentPolicy::Scope < Struct.new(:user, :scope)
-  def resolve
-    scope
+  class CommentPolicy < Struct.new(:user, :comment); end
+  class CommentPolicy::Scope < Struct.new(:user, :scope)
+    def resolve
+      scope
+    end
   end
-end
-class Comment; extend ActiveModel::Naming; end
+  class Comment; extend ActiveModel::Naming; end
 
-class Article; end
+  class Article; end
 
-class BlogPolicy < Struct.new(:user, :blog); end
-class Blog; end
-class ArtificialBlog < Blog
-  def self.policy_class
-    BlogPolicy
+  class BlogPolicy < Struct.new(:user, :blog); end
+  class Blog; end
+  class ArtificialBlog < Blog
+    def self.policy_class
+      BlogPolicy
+    end
   end
-end
-class ArticleTag
-  def self.policy_class
-    Struct.new(:user, :tag) do
-      def show?
-        true
-      end
-      def destroy?
-        false
+  class ArticleTag
+    def self.policy_class
+      Struct.new(:user, :tag) do
+        def show?
+          true
+        end
+        def destroy?
+          false
+        end
       end
     end
   end
 end
-
-SpecHelper.pundit_examples(self, self.class)
+SpecHelper.pundit_examples(self, AS)
