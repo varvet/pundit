933d9295ba80473faf0ee96e0afc8620c14098f5
John Otander <johnotander@gmail.com>
2014-04-22 16:11:17 -0600

    Add spec generator for policies.
    Add test hook.
    
    Remove old spec template.

---
+++ lib/generators/rspec/templates/policy_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe <%= class_name %>Policy do
+
+  let(:user) { User.new }
+
+  subject { <%= class_name %>Policy }
+
+  permissions ".scope" do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :create? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :show? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :update? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :destroy? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
