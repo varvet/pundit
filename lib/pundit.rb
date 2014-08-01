73e35625ec2fbe724bb08d4ed1b0b5bd0922b1e3
Michal Gilewski <psajcho@gmail.com>
2014-08-01 12:20:23 +0200

    fixed module recognition

--- lib/pundit.rb
+++ lib/pundit.rb
@@ -84,7 +84,7 @@ module Pundit
   attr_writer :policy_scope
 
   def policy(record)
-    namespace = self.class.deconstantize
+    namespace = record.try(:name).try(:deconstantize).try(:constantize) || self.class.parent
     @_policy or Pundit.policy!(pundit_user, record, namespace)
   end
 
