386cae5f0765dcbe063e44d288a4ccbe196b7fa0
Duncan Stuart <dgmstuart@gmail.com>
2019-08-21 17:10:23 +0200

    Make generators comply with Rubocop
    There doesn't seem to be any particular reason to exclude these. It's at
    least nice to have consistent use of quotes within the same file.
    
    This does run the risk of imposing our specific rubocop rules on other
    people's code (double vs single quotes is the cause of holy wars), but
    if anything obviously awful comes up, we can exclude generators from
    that specific rule.
    
    We do however need to exclude the templates, since these have a .rb
    extension despite containing erb tags.

--- lib/generators/pundit/policy/policy_generator.rb
+++ lib/generators/pundit/policy/policy_generator.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Pundit
   module Generators
     class PolicyGenerator < ::Rails::Generators::NamedBase
-      source_root File.expand_path('templates', __dir__)
+      source_root File.expand_path("templates", __dir__)
 
       def create_policy
-        template 'policy.rb', File.join('app/policies', class_path, "#{file_name}_policy.rb")
+        template "policy.rb", File.join("app/policies", class_path, "#{file_name}_policy.rb")
       end
 
       hook_for :test_framework
--- lib/generators/rspec/policy_generator.rb
+++ lib/generators/rspec/policy_generator.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Rspec
   module Generators
     class PolicyGenerator < ::Rails::Generators::NamedBase
-      source_root File.expand_path('templates', __dir__)
+      source_root File.expand_path("templates", __dir__)
 
       def create_policy_spec
-        template 'policy_spec.rb', File.join('spec/policies', class_path, "#{file_name}_policy_spec.rb")
+        template "policy_spec.rb", File.join("spec/policies", class_path, "#{file_name}_policy_spec.rb")
       end
     end
   end
