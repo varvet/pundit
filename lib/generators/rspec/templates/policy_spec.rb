1a231d21927804b897aacc48af3bf7637bbb2bce
Dmitry Babenko <stgeneral@users.noreply.github.com>
2021-08-08 15:45:04 +0300

    Policy spec template fix for Style/StringLiterals

--- lib/generators/rspec/templates/policy_spec.rb
+++ lib/generators/rspec/templates/policy_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe <%= class_name %>Policy, type: :policy do
 
   subject { described_class }
 
-  permissions ".scope" do
+  permissions '.scope' do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 
