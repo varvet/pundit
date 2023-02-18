f404b43a0c19d87e8e5fc1e8b32cbdab6f3c2c4a
Sean Devine <sean-devine@x-b-e.com>
2023-02-18 14:21:11 +0000

    feat: Add customizable permit description

--- lib/pundit/rspec.rb
+++ lib/pundit/rspec.rb
@@ -5,6 +5,10 @@ module Pundit
     module Matchers
       extend ::RSpec::Matchers::DSL
 
+      def self.description(&block)
+        @description = block.call
+      end
+
       # rubocop:disable Metrics/BlockLength
       matcher :permit do |user, record|
         match_proc = lambda do |policy|
@@ -31,6 +35,14 @@ module Pundit
           was_were = @violating_permissions.count > 1 ? "were" : "was"
           "Expected #{policy} not to grant #{permissions.to_sentence} on " \
           "#{record} but #{@violating_permissions.to_sentence} #{was_were} granted"
+        end
+
+        description do
+          if Pundit::RSpec::Matchers.instance_variable_defined?(:@description)
+            Pundit::RSpec::Matchers.instance_variable_get(:@description)
+          else
+            super()
+          end
         end
 
         if respond_to?(:match_when_negated)
