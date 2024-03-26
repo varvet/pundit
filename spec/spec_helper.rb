4f4127876865827788acdc298e7149208897edd4
Kim Burgestrand <kim@burgestrand.se>
2024-03-26 21:08:43 +0100

    Add missing test around cache usage
    This is not strictly a feature that was intended, but it's how it works so it needs to be supported in the future.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,8 +18,31 @@ require "active_support/core_ext"
 require "active_model/naming"
 require "action_controller/metal/strong_parameters"
 
+module InstanceTracking
+  module ClassMethods
+    def instances
+      @instances || 0
+    end
+
+    attr_writer :instances
+  end
+
+  def self.prepended(other)
+    other.extend(ClassMethods)
+  end
+
+  def initialize(*args, **kwargs, &block)
+    self.class.instances += 1
+    super(*args, **kwargs, &block)
+  end
+end
+
 class BasePolicy
+  prepend InstanceTracking
+
   class BaseScope
+    prepend InstanceTracking
+
     def initialize(user, scope)
       @user = user
       @scope = scope
